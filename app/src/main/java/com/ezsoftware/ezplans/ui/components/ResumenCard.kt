51f58ce6423c4370e13fd539e590ebcef938aea0
Mauricio <mauriciotr190@gmail.com>
2025-06-12 17:17:17 -0600

    Tab Deudas | Mejora altura dinamica CardsResumen

--- app/src/main/java/com/ezsoftware/ezplans/ui/components/ResumenCard.kt
+++ app/src/main/java/com/ezsoftware/ezplans/ui/components/ResumenCard.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
 
--- app/src/main/java/com/ezsoftware/ezplans/ui/components/VistaDetalladaPlanTabs.kt
+++ app/src/main/java/com/ezsoftware/ezplans/ui/components/VistaDetalladaPlanTabs.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
